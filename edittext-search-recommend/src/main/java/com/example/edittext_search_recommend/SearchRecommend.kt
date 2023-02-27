9cf7c628172b1f3f50364fef91051f581c43c3a5
Tushar M Shetty <tushar.shetty@7edge.com>
2023-02-27 15:17:00 +0530

    Changed implementation from method to class

--- edittext-search-recommend/src/main/java/com/example/edittext_search_recommend/SearchRecommend.kt
+++ edittext-search-recommend/src/main/java/com/example/edittext_search_recommend/SearchRecommend.kt
@@ -9,78 +9,88 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import androidx.core.widget.addTextChangedListener
 
-private var height = 0
-private var count = 0
-private lateinit var context: Context
-private lateinit var recommendList: Array<String>
-private lateinit var editText: EditText
-private var DELAY_MILLI_SEC: Long = 3000
-private var hint = ""
-fun SearchRecommend(cont: Context,eT: EditText,recList: Array<String>,initialEditTextHint: String,delayForRecommendationInSecs: Long){
-    editText = eT
-    context = cont
-    recommendList = recList
-    hint = initialEditTextHint
-    DELAY_MILLI_SEC = delayForRecommendationInSecs
-    editText.viewTreeObserver.addOnGlobalLayoutListener {
-        val imm by lazy { context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager }
-        val windowHeightMethod = InputMethodManager::class.java.getMethod("getInputMethodWindowVisibleHeight")
-        height = windowHeightMethod.invoke(imm) as Int
-        if (height == 0){
-            if ( editText.text.isEmpty()){
-                animateSearchRecommendations()
-            }
-        }else{
-            editText.hint = hint
-            editText.clearAnimation()
+class SearchRecommend{
+    companion object{
+        private var height = 0
+        private var count = 0
+        private lateinit var context: Context
+        private lateinit var recommendList: Array<String>
+        private lateinit var editText: EditText
+        private var DELAY_MILLI_SEC: Long = 3000
+        private var hint = "Search your need here..."
+        fun setInitialEditTextHint(editTextHint: String){
+            hint = editTextHint
         }
-    }
-
-    editText.addTextChangedListener {
-        if (it.toString().isEmpty()){
-            editText.hint = hint
+        fun setDelayForRecommendationInMilliSecs(delayForRecommendationInSecs: Long){
+            DELAY_MILLI_SEC = delayForRecommendationInSecs
         }
-    }
-}
+        fun searchRecommend(cont: Context, eT: EditText, recList: Array<String>){
+            editText = eT
+            context = cont
+            recommendList = recList
 
-private fun animateSearchRecommendations(){
-    Handler(Looper.getMainLooper()).postDelayed({
-        if (height == 0){
-            val anim1 = AnimationUtils.loadAnimation(context,com.google.android.material.R.anim.abc_slide_out_top)
-            val anim2 = AnimationUtils.loadAnimation(context,com.google.android.material.R.anim.abc_slide_in_bottom)
-            editText.startAnimation(anim1)
-            anim1.setAnimationListener(object : Animation.AnimationListener {
-                override fun onAnimationStart(animation: Animation?) {
-                    //
-                }
-
-                override fun onAnimationEnd(animation: Animation?) {
-                    editText.hint = "Search \"${recommendList[count]}\""
-                    editText.startAnimation(anim2)
-                }
-
-                override fun onAnimationRepeat(animation: Animation?) {
-                    //
-                }
-            })
-
-            anim2.setAnimationListener(object : Animation.AnimationListener {
-                override fun onAnimationStart(animation: Animation?) {
-                    //
-                }
-
-                override fun onAnimationEnd(animation: Animation?) {
-                    animateSearchRecommendations()
-                    count++
-                    if (count > 3){
-                        count = 0
+            editText.hint = hint
+            editText.isCursorVisible = false
+            editText.viewTreeObserver.addOnGlobalLayoutListener {
+                val imm by lazy { context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager }
+                val windowHeightMethod = InputMethodManager::class.java.getMethod("getInputMethodWindowVisibleHeight")
+                height = windowHeightMethod.invoke(imm) as Int
+                if (height == 0){
+                    if ( editText.text.toString().isEmpty()){
+                        animateSearchRecommendations()
                     }
+                }else{
+                    editText.hint = hint
+                    editText.clearAnimation()
                 }
+            }
 
-                override fun onAnimationRepeat(animation: Animation?) {
-                    //
+            editText.addTextChangedListener {
+                if (it.toString().isEmpty()){
+                    editText.hint = hint
                 }
-            })
+            }
         }
-    }, DELAY_MILLI_SEC)
+        private fun animateSearchRecommendations(){
+            Handler(Looper.getMainLooper()).postDelayed({
+                if (height == 0){
+                    val anim1 = AnimationUtils.loadAnimation(context,com.google.android.material.R.anim.abc_slide_out_top)
+                    val anim2 = AnimationUtils.loadAnimation(context,com.google.android.material.R.anim.abc_slide_in_bottom)
+                    editText.startAnimation(anim1)
+                    anim1.setAnimationListener(object : Animation.AnimationListener {
+                        override fun onAnimationStart(animation: Animation?) {
+                            //
+                        }
+
+                        override fun onAnimationEnd(animation: Animation?) {
+                            editText.hint = "Search \"${recommendList[count]}\""
+                            editText.startAnimation(anim2)
+                        }
+
+                        override fun onAnimationRepeat(animation: Animation?) {
+                            //
+                        }
+                    })
+
+                    anim2.setAnimationListener(object : Animation.AnimationListener {
+                        override fun onAnimationStart(animation: Animation?) {
+                            //
+                        }
+
+                        override fun onAnimationEnd(animation: Animation?) {
+                            animateSearchRecommendations()
+                            count++
+                            if (count > 3){
+                                count = 0
+                            }
+                        }
+
+                        override fun onAnimationRepeat(animation: Animation?) {
+                            //
+                        }
+                    })
+                }
+            }, DELAY_MILLI_SEC)
+        }
+    }
 }
